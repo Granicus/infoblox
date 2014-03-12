2284ea2bd1fe063307c1a2e139168444bc984b3f
Rhys Rhaven <rhys@rhavenindustrys.com>
2014-03-12 00:25:30 -0500

    some style changes to make it more readable

--- lib/infoblox/resource/search.rb
+++ lib/infoblox/resource/search.rb
@@ -5,12 +5,19 @@ module Infoblox
     # does not return objects not implemented in this library
     def self.find(connection,params)
       response = JSON.parse(connection.get(resource_uri,params).body)
-      response.map { |jobj| klass = resource_map[jobj["_ref"].split("/").first]; klass.nil? ? nil: klass.new(jobj) }.compact
+      response.map do |jobj| 
+        klass = resource_map[jobj["_ref"].split("/").first]
+        if klass.nil?
+          nil
+        else
+          klass.new(jobj.merge({:connection => connection}))
+        end
+      end.compact
     end
     
     # @private 
     def self.resource_map
-      resource_classes = Infoblox.constants.collect do |c|
+      resource_classes = Infoblox.constants.map do |c|
                            Infoblox.const_get(c)
                          end.select do |c|
                            c.is_a?(Class) && c < Infoblox::Resource
