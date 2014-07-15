428cf02c21d45af777c485ac01be4909d0f134a0
Tony Notto <Tony.Notto@target.com>
2014-07-15 12:28:51 -0500

    Add ipv4address resource

---
+++ lib/infoblox/resource/ipv4address.rb
@@ -0,0 +1,20 @@
+module Infoblox
+  class Ipv4address < Resource
+    attr_accessor :dhcp_client_identifier, :ip_address, :is_conflict, 
+                  :lease_state, :mac_address, :names, :network, 
+                  :network_view, :objects, :status, :types, :usage, 
+                  :username
+    
+    wapi_object "ipv4address"
+
+    def delete
+      raise "Not supported"
+    end
+    def create
+      raise "Not supported"
+    end
+    def modify
+        raise "Not supported"
+    end
+  end
+end
