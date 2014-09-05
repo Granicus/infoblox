3b4491b670652d2167bc14eee3a01179590b358c
Kevin Mullin <kevin@kpmullin.com>
2014-09-04 17:32:06 -0700

    Add comment field to cname resource

--- lib/infoblox/resource/cname.rb
+++ lib/infoblox/resource/cname.rb
@@ -1,6 +1,6 @@
 module Infoblox
   class Cname < Resource
-    remote_attr_accessor :name, :canonical, 
+    remote_attr_accessor :name, :canonical, :comment,
                          :extensible_attributes, :view
 
     wapi_object "record:cname"
