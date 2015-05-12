b1b39740ef560ad81e71a77f6287550131b545c2
prabhu-das <prabhu.das@clogeny.com>
2015-05-12 03:05:55 -0700

    Added support for zone resource.

---
+++ lib/infoblox/resource/zone.rb
@@ -0,0 +1,9 @@
+module Infoblox 
+  class Zone < Resource
+    
+    attr_accessor :fqdn 
+    remote_attr_accessor :address, :comment, :disable, :network_view, :extattrs
+    
+    wapi_object "zone"
+  end
+end
