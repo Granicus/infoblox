0351abebc2df5a70f14108b6b3d0d418e0e0be3c
prabhu-das <prabhu.das@clogeny.com>
2015-05-12 23:44:13 -0700

    Updated zone_auth resource with correct wapi_object reference.

--- lib/infoblox/resource/zone_auth.rb
+++ lib/infoblox/resource/zone_auth.rb
@@ -1,8 +1,8 @@
 module Infoblox 
-  class Zone < Resource
+  class ZoneAuth < Resource
     attr_accessor :fqdn 
     remote_attr_accessor :address, :comment, :disable, :network_view, :extattrs
     
-    wapi_object "zone"
+    wapi_object "zone_auth"
   end
 end
