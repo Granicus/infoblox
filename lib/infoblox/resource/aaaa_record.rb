91ed1b8c95349f62a857d6f97a9f9e1e6201bc91
prabhu-das <prabhu.das@clogeny.com>
2015-05-14 01:45:05 -0700

    Updated aaaa record attributes with a comment.

--- lib/infoblox/resource/aaaa_record.rb
+++ lib/infoblox/resource/aaaa_record.rb
@@ -2,7 +2,8 @@
 module Infoblox
   class AAAArecord < Resource
     remote_attr_accessor :name, :ipv6addr, :ttl,
-                         :extattr, :view, :comment, :disable, :dns_name, :use_ttl, :zone
+                         :extattr, :view, :comment, :disable, :dns_name, :use_ttl, :zone,
+                         :extensible_attributes # keeping both formats of extensible attributes to support all WAPI versions
 
     wapi_object "record:aaaa"
   end
