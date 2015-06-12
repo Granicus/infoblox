57fc889e03b81eff8037f888f124ee7fd9ef1912
Klaas Demter <demter@atix.de>
2015-06-12 14:45:00 +0200

    Change Ipv4address behaviour to use remote_attr_accessor

--- lib/infoblox/resource/ipv4address.rb
+++ lib/infoblox/resource/ipv4address.rb
@@ -1,18 +1,18 @@
 module Infoblox
   class Ipv4address < Resource
-    attr_accessor :dhcp_client_identifier, 
-                  :ip_address, 
-                  :is_conflict, 
-                  :lease_state, 
-                  :mac_address, 
-                  :names, 
-                  :network, 
-                  :network_view, 
-                  :objects, 
-                  :status, 
-                  :types, 
-                  :usage, 
-                  :username
+    remote_attr_accessor :dhcp_client_identifier, 
+                         :ip_address, 
+                         :is_conflict, 
+                         :lease_state, 
+                         :mac_address, 
+                         :names, 
+                         :network, 
+                         :network_view, 
+                         :objects, 
+                         :status, 
+                         :types, 
+                         :usage, 
+                         :username
     
     wapi_object "ipv4address"
 
